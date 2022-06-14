af2e63903f531cb61b086126b01f8b5fdcc28f19
iOSVijay <applevj89@gmail.com>
2022-06-14 14:17:07 +0530

    Initial Commit

---
+++ GDC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GDC
+//
+//  Created by Mac on 14/06/22.
+//  Copyright © 2022 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
